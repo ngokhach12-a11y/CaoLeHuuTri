9377faaa34fc0d34b24c7d10930fc7c38bddec4b
ngokhach12-a11y <ngokhach12@gmail.com>
2025-12-03 02:11:08 +0700

    Create post_model.dart

---
+++ lib/post_model.dart
@@ -0,0 +1,8 @@
+// File: lib/post_model.dart
+class Post {
+  final String name;    // Tên người đăng
+  final String content; // Nội dung status
+  final String time;    // Thời gian đăng
+
+  Post({required this.name, required this.content, required this.time});
+}
