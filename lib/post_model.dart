2f530044cb643568d1ed428484af01fe8e3a675c
ngokhach12-a11y <ngokhach12@gmail.com>
2025-12-03 02:13:24 +0700

    Delete lib directory

--- lib/post_model.dart
+++
@@ -1,8 +0,0 @@
-// File: lib/post_model.dart
-class Post {
-  final String name;    // Tên người đăng
-  final String content; // Nội dung status
-  final String time;    // Thời gian đăng
-
-  Post({required this.name, required this.content, required this.time});
-}
